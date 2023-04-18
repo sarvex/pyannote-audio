0ad92f0d0a73d500d8190f79855960f60c6db287
Hervé BREDIN <hbredin@users.noreply.github.com>
2023-04-18 15:10:21 +0200

    fix: add missing import

--- pyannote/audio/models/segmentation/__init__.py
+++ pyannote/audio/models/segmentation/__init__.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2020 CNRS
+# Copyright (c) 2020- CNRS
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from .GuidedPyanNet import GuidedPyanNet
 from .PyanNet import PyanNet
 
-__all__ = ["PyanNet"]
+__all__ = ["PyanNet", "GuidedPyanNet"]
